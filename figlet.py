85b196e4e682942d468b08c91c3eed0ea995d66a
lilcryptotech <72393350+seandadonntech@users.noreply.github.com>
2022-09-25 07:51:09 -0400

    Update figlet.py

--- figlet.py
+++ figlet.py
@@ -2,6 +2,6 @@ import pyfiglet
 from colorama import Fore, Back, Style
 print(Fore.GREEN + 'a  program that genrates text into pyfiglet  made by seandadonntech , technio#7716, ')
 input = input("what text to you what to print out?\n")
-result = pyfiglet.figlet_format(f"{input}", font = "slant"  )
+result = pyfiglet.figlet_format(f"{input}", font = "slant"  ) # feel free to change this to whatever you want
 print(result)
-
+#THIS CODE IS OWNED BY SEANDADONNTECH 
