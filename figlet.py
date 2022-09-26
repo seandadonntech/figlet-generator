eaa0adbd4646bb816cd9de3c321d5a58de3852aa
lilcryptotech <72393350+seandadonntech@users.noreply.github.com>
2022-09-25 23:09:27 -0400

    Update figlet.py

--- figlet.py
+++ figlet.py
@@ -1,9 +1,11 @@
 import pyfiglet
+# made by seandadonntech , technio#7716, aka lilcryptotech
 from colorama import Fore, Back, Style
-print(Fore.GREEN + 'a  program that genrates text into pyfiglet  made by seandadonntech , technio#7716, ')
-input = input("what text to you what to print out?\n")
+print(Fore.GREEN + 'A  program that genrates text into pyfiglet  made by seandadonntech , technio#7716, ')
 direction = input("what options of text do you to print? options available are slant\n")
- if direction == "slant": # this is an argument/ IM ADDING MORE OPTIONS FOR THIS PROGRAM
-result = pyfiglet.figlet_format(f"{input}", font = "slant"  ) # feel free to change this to whatever you want
+if direction == "slant": # this is an argument/ IM ADDING MORE OPTIONS FOR THIS PROGRAM
+ input = input("what text to you what to print out?\n")
+result = pyfiglet.figlet_format(f"{input}", font = "slant" )
+
+
 print(result)
-#THIS CODE IS OWNED BY SEANDADONNTECH 
