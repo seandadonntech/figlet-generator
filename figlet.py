cb814234957ab41612494abec5b60356bde1c96c
lilcryptotech <72393350+seandadonntech@users.noreply.github.com>
2022-09-27 17:40:30 -0400

    Update figlet.py
    more fonts coming soon

--- figlet.py
+++ figlet.py
@@ -8,4 +8,11 @@ if direction == "slant": # this is an argument/ IM ADDING MORE OPTIONS FOR THIS 
 result = pyfiglet.figlet_format(f"{input}", font = "slant" )
 
 
+
 print(result)
+if  direction == "3-d":
+ input = input("what text do you want to print out in 3-d mode?")
+ result = pyfiglet.figlet_format(f"{input}",  font = "3-d")
+ print(result)
+
+ 
