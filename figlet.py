efb4e4007a2d746ad2ef0a6e811f0c9b0b954690
lilcryptotech <72393350+seandadonntech@users.noreply.github.com>
2022-11-13 16:20:34 -0500

    Update figlet.py

--- figlet.py
+++ figlet.py
@@ -1,38 +1,44 @@
-import pyfiglet
+
+
 # made by seandadonntech , technio#7716, aka lilcryptotech
-from colorama import Fore, Back, Style
+from time import *
+
+import pyfiglet
+from colorama import Back, Fore, Style
+
 print(Fore.GREEN + 'A  program that genrates text into pyfiglet  made by seandadonntech , technio#7716, Copyright (c) 2022, lilcryptotech All rights reserved. ')
-direction = input("what options of text do you to print? options available are slant\n")
+
+
+
+direction = input("what options of text do you to print? don't know type help for list of commands \n")
+#----------------------------------------------
+#slant mode
+#------------------------------------------------------------------------------------------------------------------------
 if direction == "slant": # this is an argument/ IM ADDING MORE OPTIONS FOR THIS PROGRAM
- input = input("what text to you what to print out?\n")
+  input = input("what text to you what to print out in slant mode?\n")
 result = pyfiglet.figlet_format(f"{input}", font = "slant" )
-
-
-
 print(result)
-if  direction == "3-d":
- input = input("what text do you want to print out in 3-d mode?")
- result = pyfiglet.figlet_format(f"{input}",  font = "3-d")
- print(result)
+#---------------------------------------------------------------
+if direction == "3-d":
+  input = input("What text do you want to print out in 3-d mode?\n")
+result = pyfiglet.figlet_format(f"{input}", font = "3-d")
+print(result)
 
- 
-if direction == "doom":
- input = input("what text do you want to put in doom mode?")
- doom = pyfiglet.figlet_format(f"{input}", font = "doom")
-print(doom)
+if direction == "doh":
+ input = input("What text to you want to print out in doh mode?")
+result = pyfiglet.figlet_format(f"{input}", font = "doh")
+print(result)
 
-if direction == "roman":
- input = input("what text do you want to in roman mode")
- roman = pyfiglet.figlet_format(f"{input}", font = "roman")
- print(roman)
-if direction == "flag":
- input = input("what text do you what in usa flag mode?")
- flag = pyfiglet.figlet_format(f"{input}", font = "usaflag")
- print(flag)
- if direction == "computer":
- input = input('what text do you want to put in comoputer mode\n')
-computer = pyfiglet.figlet_format(f"{input}", font = "computer" )
-print(computer)
+if direction =="thin":
+ input = input("What text you want to print thin mode")
+ result = pyfiglet.figlet_format(f"{input}", font = "thin")
+print(result)
 
-print(f"  you chose was {direction} ")
+if direction == "3d-banner":
+ input = input("What text do you want to in banner3-D")
+ result = pyfiglet.figlet_format(f"{input}", font = "banner3-D")
+print(result)
+print(f"you choose {direction} mode")
+
+
 
